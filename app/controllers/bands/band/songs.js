73c1c9637922da557d35846c97fb3e16273b302d
Tobias Brysiewicz <tbrysiewicz@att.net>
2020-02-23 15:10:15 -0600

    page 122

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -24,9 +24,8 @@ export default Controller.extend({
     this.set('newSongTitle', '');
   }),
 
-  updateRating: action(function(params) {
-    let { item: song, rating } = params;
-    song.set('rating', rating);
+  updateRating: action(function(song, rating) {
+    song.set('rating', song.rating === rating ? 0 : rating);
   })
 
 });
