0fbde18963fd17e68eef50e09afc43bba85d49aa
Tobias Brysiewicz <tbrysiewicz@att.net>
2020-02-23 11:55:04 -0600

    page 90

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,9 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import Band from 'rarwe/models/band';
+import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: '',
+
+  isAddButtonDisabled: empty('newBandName'),
 
   addBand: action(function(){
     this.set('isAddingBand', true);
@@ -13,8 +17,11 @@ export default Controller.extend({
     this.set('isAddingBand', false);
   }),
 
-  saveBand: action(function(){
-
+  saveBand: action(function(event){
+    event.preventDefault();
+    let newBand = Band.create({ name: this.newBandName });
+    this.model.pushObject(newBand);
+    this.set('newBandName', '');
   }),
 
 });
