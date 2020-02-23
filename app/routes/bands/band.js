f45a3b9cde6a413c1a36a34423bfc151ec7fab48
Tobias Brysiewicz <tbrysiewicz@att.net>
2020-02-23 16:26:19 -0600

    page 138

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -5,6 +5,7 @@ export default Route.extend({
   router: service(),
 
   model(params) {
+    console.log('Model hook called for `bands.band` called with', params.slug);
     let bands = this.modelFor('bands');
     return bands.find(band => band.slug === params.slug);
   },
