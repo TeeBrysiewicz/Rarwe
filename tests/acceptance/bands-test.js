3d21d536eb3f8c3e833d72995167639e068bc8eb
Tobias Brysiewicz <tbrysiewicz@att.net>
2020-02-25 12:01:00 -0600

    page 191

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
-import { visit, click, fillIn } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
+import { createBand } from 'rarwe/tests/helpers/custom-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
@@ -13,23 +14,19 @@ module('Acceptance | Bands', function(hooks) {
 
     await visit('/');
 
-    let bandLinks = document.querySelectorAll('.rr-band-link');
-    assert.equal(bandLinks.length, 2, 'All band links are rendered');
-    assert.ok(bandLinks[0].textContent.includes('Radiohead'), 'First band link contains the band name');
-    assert.ok(bandLinks[1].textContent.includes('Long Distance Calling'), 'The other band link contains the band name');
+    assert.dom('[data-test-rr=band-link]').exists({ count: 2 }, 'All band links are rendered');
+    assert.dom('[data-test-rr=band-list-item]:first-child').hasText("Radiohead", 'The first band link contains the band name');
+    assert.dom('[data-test-rr=band-list-item]:last-child').hasText("Long Distance Calling", 'The other band link contains the band name');
   });
 
   test('Create a band', async function(assert) {
     this.server.create('band', { name: 'Royal Blood' });
 
     await visit('/');
-    await click('.rr-new-label button');
-    await fillIn('.rr-input', 'Caspian');
-    await click('.rr-action-button');
+    await createBand('Caspian');
 
-    let bandLinks = document.querySelectorAll('.rr-band-link');
-    assert.equal(bandLinks.length, 2, 'All band links are rendered', 'A new band link is rendered');
-    assert.ok(bandLinks[1].textContent.includes('Caspian'), 'The new band link is rendered as the last item');
-    assert.ok(document.querySelector('.rr-navbar-item > .active').textContent.includes('Songs'), 'The Songs tab is active');
+    assert.dom('[data-test-rr=band-list-item]').exists({ count: 2 }, 'A new band link is rendered');
+    assert.dom('[data-test-rr=band-list-item]:last-child').hasText('Caspian', 'The new band link is rendered as the last item');
+    assert.dom('[data-test-rr=songs-nav-item] > .active').exists('The Songs tab is active');
   });
 });
