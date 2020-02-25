591e2062ecde40f427b35b94d6dd66fa226e8d64
Tobias Brysiewicz <tbrysiewicz@att.net>
2020-02-25 17:05:43 -0600

    song tests working

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
-import { visit, click } from '@ember/test-helpers';
-import { createBand, createSong } from 'rarwe/tests/helpers/custom-helpers';
+import { visit, click, fillIn, pauseTest } from '@ember/test-helpers';
+import { createBand } from 'rarwe/tests/helpers/custom-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
@@ -9,19 +9,13 @@ module('Acceptance | Songs', function(hooks) {
   setupMirage(hooks);
 
   // test('List Songs', async function(assert) {
-
   //   let newBand = this.server.create('band', { name: 'Martin Garrix' });
-
-  //   // this.server.createList('song', 2, { band: newBand });
-
-  //   this.server.create('song', { title: 'Animals' }, { band: newBand });
-  //   this.server.create('song', { title: 'High On Life' }, { band: newBand });
+  //   this.server.create('song', { band: newBand });
 
   //   // await visit('/');
   //   await visit('/bands/1/songs');
-  //   // await click('.rr-band-link');
 
-  //   // await click('[data-test-rr=band-link]');
+  //   await pauseTest();
 
   //   assert.dom('[data-test-rr=song-list-item]').exists({ count: 2 }, 'All songs are rendered');
   //   assert.dom('[data-test-rr=song-list-item]:first-child').hasText("Animals", 'The first song contains the song name');
@@ -33,13 +27,17 @@ module('Acceptance | Songs', function(hooks) {
 
     await visit('/');
     await createBand('Tiesto');
-    // await visit('/bands/2/songs');
-    await createSong('Secrets');
-    await createSong('Money');
+
+    await click('[data-test-rr=new-song-label]');
+    await fillIn('[data-test-rr=new-song-input]', "Secrets");
+    await click('[data-test-rr=new-song-button]');
+    await click('[data-test-rr=new-song-input');
+    await fillIn('[data-test-rr=new-song-input', "Dance!");
+    await click('[data-test-rr=new-song-button]');
 
     assert.dom('[data-test-rr=song-list-item]').exists({ count: 2 }, 'All songs are rendered');
-    assert.dom('[data-test-rr=song-list-item]:first-child').hasText("Animals", 'The first song contains the song name');
-    assert.dom('[data-test-rr=song-list-item]:last-child').hasText("Secrets", 'The other contains the song name');
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText("Secrets", 'The first song contains the song name');
+    assert.dom('[data-test-rr=song-list-item]:last-child').hasText("Dance!", 'The other contains the song name');
   });
 
 });
